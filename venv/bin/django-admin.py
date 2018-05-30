4de1e5a7f6f8b90d8c964da90f32979fb41150cf
root <root@ericwangcluster1.novalocal>
2018-05-30 01:44:09 -0400

    post module 6

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hadoop/workspace/dockerAnsible/cd-docker-ansible/todobackend/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
